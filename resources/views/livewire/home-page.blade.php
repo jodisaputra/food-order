8edabcdaa3f2a21a0649a617913e084749dfd5b0
jodisaputra <116236799+jodisaputra@users.noreply.github.com>
2024-06-11 16:37:47 +0700

    Update home-page.blade.php

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -1,5 +1,6 @@
 <div>
-    <div class="w-full h-screen bg-gradient-to-r from-blue-200 to-cyan-200 py-10 px-4 sm:px-6 lg:px-8 mx-auto">
+    {{-- <div class="w-full h-screen bg-gradient-to-r from-blue-200 to-cyan-200 py-10 px-4 sm:px-6 lg:px-8 mx-auto"> --}}
+    <div class="w-full h-screen bg-white py-10 px-4 sm:px-6 lg:px-8 mx-auto">
         <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8">
             <!-- Grid -->
             <div class="grid md:grid-cols-2 gap-4 md:gap-8 xl:gap-20 md:items-center">
@@ -159,14 +160,14 @@
 
                 <div class="relative ms-4">
                     <img class="w-full rounded-md"
-                        src="https://static.vecteezy.com/system/resources/previews/011/993/278/non_2x/3d-render-online-shopping-bag-using-credit-card-or-cash-for-future-use-credit-card-money-financial-security-on-mobile-3d-application-3d-shop-purchase-basket-retail-store-on-e-commerce-free-png.png"
+                        src="https://images.pexels.com/photos/955137/pexels-photo-955137.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
                         alt="Image Description">
                     <div
                         class="absolute inset-0 -z-[1] bg-gradient-to-tr from-gray-200 via-white/0 to-white/0 w-full h-full rounded-md mt-4 -mb-4 me-4 -ms-4 lg:mt-6 lg:-mb-6 lg:me-6 lg:-ms-6 dark:from-slate-800 dark:via-slate-900/0 dark:to-slate-900/0">
                     </div>
 
                     <!-- SVG-->
-                    <div class="absolute bottom-0 start-0">
+                    {{-- <div class="absolute bottom-0 start-0">
                         <svg class="w-2/3 ms-auto h-auto text-white dark:text-slate-900" width="630"
                             height="451" viewBox="0 0 630 451" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <rect x="531" y="352" width="99" height="99" fill="currentColor" />
@@ -187,7 +188,7 @@
                             <rect x="507" y="124" width="49" height="24" fill="currentColor" />
                             <rect x="531" y="49" width="99" height="99" fill="currentColor" />
                         </svg>
-                    </div>
+                    </div> --}}
                     <!-- End SVG-->
                 </div>
                 <!-- End Col -->
